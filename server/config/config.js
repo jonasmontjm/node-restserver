7339e1d7da57cebe926a56344a76ef68adf2ff5c
jonasmont <jonathanmontjm@gmail.com>
2019-08-29 23:09:58 -0400

    Variable de Entorno

--- server/config/config.js
+++ server/config/config.js
@@ -15,10 +15,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 if (process.env.NODE_ENV === 'env') {
     urlDB = 'mongodb: //localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafeuser:7aPDTREAkWAtMKK@cafecluster-r39dp.mongodb.net/test?retryWrites=true&w=majority';
+    urlDB = process.env.nombredb //';
 }
 
-urlDB = 'mongodb+srv://cafeuser:7aPDTREAkWAtMKK@cafecluster-r39dp.mongodb.net/test?retryWrites=true&w=majority';
+
 process.env.URLDB = urlDB
 
 //
