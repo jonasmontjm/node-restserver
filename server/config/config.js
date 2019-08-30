5187971978e0159f10b2ba43644b206bd4e85037
jonasmont <jonathanmontjm@gmail.com>
2019-08-29 23:15:15 -0400

    Actualizacion Variable Entorno

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 if (process.env.NODE_ENV === 'env') {
     urlDB = 'mongodb: //localhost:27017/cafe';
 } else {
-    urlDB = process.env.nombredb //';
+    urlDB = process.env.MONGO_URI //';
 }
 
 
